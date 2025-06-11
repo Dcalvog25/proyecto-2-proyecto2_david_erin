56fa34255d03b9c9bb99ec5efe73903b7517eeb0
Dcalvog25 <davidcg2508@gmail.com>
2025-06-11 15:58:38 -0600

    Avances en interfaz
    Se realizaron avances en interfaz "home page".

---
+++ proyectos/ProyectoSistemaHotelero/ProyectoSistemaHotelero/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoSistemaHotelero.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
